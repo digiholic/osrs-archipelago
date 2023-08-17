4c78c87e312971c596e9dcd29a833dceb4f60899
digiholic <digikun@gmail.com>
2023-08-16 19:16:28 -0600

    Adds popup message system when receiving items

--- src/main/java/com/archipelago/ArchipelagoPlugin.java
+++ src/main/java/com/archipelago/ArchipelagoPlugin.java
@@ -8,6 +8,7 @@ import com.google.inject.Provides;
 import javax.inject.Inject;
 import javax.swing.*;
 
+import com.sun.jna.platform.unix.X11;
 import gg.archipelago.client.ClientStatus;
 import gg.archipelago.client.LocationManager;
 import lombok.extern.slf4j.Slf4j;
@@ -15,6 +16,7 @@ import net.runelite.api.*;
 import net.runelite.api.events.*;
 import net.runelite.api.widgets.Widget;
 import net.runelite.api.widgets.WidgetInfo;
+import net.runelite.api.widgets.WidgetModalMode;
 import net.runelite.client.callback.ClientThread;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
@@ -65,6 +67,9 @@ public class ArchipelagoPlugin extends Plugin
 	//This boolean will become true when we log in, and will be set back to false in the first game tick.
 	//This lets us check if the logged in player should auto-connect to AP
 	private boolean justLoggedIn = false;
+
+	private Queue<String[]> queuedMessages = new LinkedList<>();
+	private boolean isDisplayingPopup = false;
 
 	@Provides
 	ArchipelagoConfig provideConfig(ConfigManager configManager)
@@ -165,10 +170,41 @@ public class ArchipelagoPlugin extends Plugin
 			}
 			justLoggedIn = false;
 		}
-		if (connected){
+		if (loggedIn && connected){
 			checkStatus();
 			SendChecks();
+
+			if (!isDisplayingPopup && queuedMessages.size() > 0){
+				String[] msg = queuedMessages.poll();
+				isDisplayingPopup = true;
+				clientThread.invokeLater(() -> {
+					DisplayPopupMessage(msg[0],msg[1]);
+				});
+			}
 		}
+	}
+
+	public void QueuePopupMessage(String header, String body){
+		log.info("Queueing popup message for "+body);
+		queuedMessages.add(new String[]{header, body});
+	}
+	private void DisplayPopupMessage(String header, String body){
+		WidgetNode widgetNode = client.openInterface((161 << 16) | 13, 660, WidgetModalMode.MODAL_CLICKTHROUGH);
+		log.info("Opening popup message for "+body);
+		client.runScript(3343, header, body, -1);
+
+		clientThread.invokeLater(() -> {
+			Widget w = client.getWidget(660, 1);
+			if (w.getWidth() > 0) {
+				return false;
+			}
+
+			client.closeInterface(widgetNode, true);
+			log.info("Closing interface message for "+body);
+			isDisplayingPopup = false;
+			return true;
+		});
+
 	}
 
 	@Subscribe
@@ -255,6 +291,8 @@ public class ArchipelagoPlugin extends Plugin
 			apClient.sendChat(cmd);
 			log.info("Sending string to AP: "+cmd);
 			chatboxInput.consume();
+
+			clientThread.invokeLater((() -> DisplayPopupMessage("Test 1", "Test 2")));
 		}
 	}
 
--- src/main/java/com/archipelago/apEvents/ReceiveItem.java
+++ src/main/java/com/archipelago/apEvents/ReceiveItem.java
@@ -5,6 +5,8 @@ import com.archipelago.ItemHandler;
 import gg.archipelago.client.events.ArchipelagoEventListener;
 import gg.archipelago.client.events.ReceiveItemEvent;
 
+import java.text.MessageFormat;
+
 public class ReceiveItem {
 
     @ArchipelagoEventListener
@@ -12,6 +14,14 @@ public class ReceiveItem {
         if (event.getIndex() >= ArchipelagoPlugin.plugin.lastItemReceivedIndex){
             ArchipelagoPlugin.plugin.addCollectedItem(ItemHandler.ItemsById.get(event.getItemID()));
             ArchipelagoPlugin.plugin.lastItemReceivedIndex = event.getIndex();
+            //Don't send messages for items received on startup:
+            if (event.getIndex() > 0){
+                String messageBody = "Received from " +
+                        event.getPlayerName() +
+                        " at " +
+                        event.getLocationName();
+                ArchipelagoPlugin.plugin.QueuePopupMessage(event.getItemName(), messageBody);
+            }
         }
     }
 }
